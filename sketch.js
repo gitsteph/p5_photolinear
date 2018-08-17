3ebb9334ca06490865f20f02bad4255583b1e4c7
Stephanie H. Andrews <byronic.string@gmail.com>
2018-08-17 15:23:34 -0700

    added neat sliders

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 var img;
+var strokeThickness;
+var strokeLength;
+var strokeThicknessSlider;
+var strokeLengthSlider;
 
 
 function preload() {
@@ -20,9 +24,9 @@ function scribbleLines(targetImg) {
                 var g = targetImg.pixels[pixelValueBase + 1];
                 var b = targetImg.pixels[pixelValueBase + 2];
                 var a = 200;
-                var x2 = x1 - 20;
-                var y2 = y1 + 20;
-                strokeWeight(2);
+                var x2 = x1 - strokeLength;
+                var y2 = y1 + strokeLength;
+                strokeWeight(strokeThickness);
                 stroke(r, g, b, 200);
                 scribble.bowing = random(0, 1, 0.1);
                 scribble.roughness = 1.5;
@@ -36,9 +40,28 @@ function scribbleLines(targetImg) {
 function setup() {
     img.resize(0, Math.min((windowHeight - 100), 480));
     img.loadPixels();
+
+    strokeThicknessSlider = createSlider(1, 50, 5, 1);
+    strokeThicknessSlider.style('width', '120px');
+    strokeThicknessSlider.position(5, img.height + 50);
+
+    strokeLengthSlider = createSlider(5, 50, 25, 1);
+    strokeLengthSlider.style('width', '120px');
+    strokeLengthSlider.position(5, img.height + 100);
+
     scribbleLines(img);
 }
 
 function draw() {
+    strokeThickness = strokeThicknessSlider.value(); // 2 default
+    strokeLength = strokeLengthSlider.value() // 25 default
+
     scribbleLines(img);
+
+    textSize(50);
+    fill(255,0,255, 180);
+    stroke(255,103,0);
+    strokeWeight(2);
+    text("stroke thickness", strokeThicknessSlider.x * 2 + strokeThicknessSlider.width, strokeThicknessSlider.y + strokeThicknessSlider.height / 2);
+    text("stroke length", strokeLengthSlider.x * 2 + strokeLengthSlider.width, strokeLengthSlider.y + strokeLengthSlider.height / 2);
 }
